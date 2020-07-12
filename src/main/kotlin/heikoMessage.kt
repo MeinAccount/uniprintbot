f920f19acd04696709bd1ed1c61fdd7584b802df
Magnus Kühn <magnus.khn@gmail.com>
2020-07-12 21:18:19 +0200

    Text from stdin

--- src/main/kotlin/heikoMessage.kt
+++ src/main/kotlin/heikoMessage.kt
@@ -1,12 +1,14 @@
 import heiko.HeikoNotificationBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import print.UniPrintBot
 
 fun main(args: Array<String>) {
     val bot = HeikoNotificationBot()
 //    bot.execute(SendSticker().setSticker("CAADAgADdAYAApb6EgVv0K8Liwm_rQI").setChatId(HEIKO_GROUP))
 
+    print("Text to send: ")
     val text = readLine()
     if (text != null) {
-        bot.execute(SendMessage(HEIKO_GROUP, text).setParseMode("Markdown"))
+        println(bot.execute(SendMessage(HEIKO_GROUP, text).setParseMode("Markdown")))
     }
 }
